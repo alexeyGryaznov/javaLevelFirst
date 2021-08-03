c3bc181642e7ebacf3aee44b05d062edef2b801e
alexeyGryaznov <le1984@mail.ry>
2021-08-03 22:36:43 +0300

    lesson2 all tasks

--- src/main/java/ru/gb/gryaznov/lesson1/HomeWork2.java
+++ src/main/java/ru/gb/gryaznov/lesson1/HomeWork2.java
@@ -8,9 +8,21 @@ public class HomeWork2 {
         System.out.println(resultCheckSumm);
         int numForCheck = -5; //число для проверки положительное или отрицательное
         checkNum(numForCheck);
-        int posOrNeg = 0; //число для проверки отрицательное или нет с возвратом значения функцией
+        int posOrNeg = 3; //число для проверки отрицательное или нет с возвратом значения функцией
         boolean resultLogic = checkNumLogic(posOrNeg);
         System.out.println("Это число отрицательное? " + posOrNeg + " - " + resultLogic);
+        String str = "Домашка2"; // строка для печати n-раз
+        int strRepit = 3;// сколько раз печатать
+        printStr(str,strRepit);// метод печати строки
+        int year = 1200;
+        boolean checkYr = checkYear(year);
+        System.out.println(checkYr);
+        if(checkYr == true){
+            System.out.println(year + " - високосный");
+        }else {
+            System.out.println(year + "- не високосный");
+        }
+
     }
 public static boolean checkSumm(int a,int b){
     boolean result;
@@ -30,6 +42,41 @@ public static void checkNum(int a){
 public static boolean checkNumLogic(int a){
         boolean result;
 if(a<0){result=true;}else{result=false;}
+return result;
+}
+public static void printStr(String a, int b){
+        switch (b){
+            case 0:
+                System.out.println("Эта строка: " + a + " НЕ будет напечатана т.к. введен 0");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Эта строка: " + a + " будет напечатана "+ b + " раза:");
+                break;
+                default:
+                System.out.println("Эта строка: " + a +" будет напечатана "+ b + " раз:");
+        }
+    System.out.println();
+    for(int i=0; i<b; i++){
+        System.out.println(a);
+    }
+}
+public static boolean checkYear(int a){
+boolean result = false;
+switch (a%100) {
+    case 0:
+        if (a % 400 == 0 && a % 4 == 0) {
+            result = true;
+        }
+        break;
+    default:
+        if (a % 4 == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+}
 return result;
 }
 }
