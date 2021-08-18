479c81a184ae2c36adda69d4abe3b09085eef12a
alexeyGryaznov <le1984@mail.ry>
2021-08-19 00:03:18 +0300

    HW6 all done

--- src/main/java/ru/gb/gryaznov/lesson6/Dog.java
+++ src/main/java/ru/gb/gryaznov/lesson6/Dog.java
@@ -1,4 +1,44 @@
 package ru.gb.gryaznov.lesson6;
 
-public class Dog {
+import java.util.Random;
+
+public class Dog extends Animal{
+    private int age;
+    private String name;
+    public static int checkNum;
+
+    public Dog(String name,int age){
+        super(name);
+        this.name=name;
+        this.age=age;
+        setCheckNum();
+    }
+
+
+    public void setCheckNum() {
+        this.checkNum = checkNum+1;
+    }
+    public int getCheckNum(){
+        return checkNum;
+    }
+
+    @Override
+    public void running(int run){
+        if(run<=500){
+        System.out.println(this.name + " пробежал: "+ run + " метров");
+
+        }else {
+            System.out.println(this.name + " пробежал: " + "500 метров и прополз еще "+(run-500) + " метров");
+        }
+    }
+    @Override
+    public void swiming(int swim){
+        if(swim<=10){
+            System.out.println(this.name + " проплыл: "+ swim + " метров");
+        }else {
+            Random rand = new Random();
+            int swimPossibility=10+rand.nextInt(this.age);
+            System.out.println(this.name + " проплыл: "+ swimPossibility + " метров и утонул");
+        }
+    }
 }
