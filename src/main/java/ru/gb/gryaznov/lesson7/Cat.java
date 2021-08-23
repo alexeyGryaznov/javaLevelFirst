446a6b393660861f57da30d9605103cb1496af50
alexeyGryaznov <le1984@mail.ry>
2021-08-23 17:16:39 +0300

    HW7_done

--- src/main/java/ru/gb/gryaznov/lesson7/Cat.java
+++ src/main/java/ru/gb/gryaznov/lesson7/Cat.java
@@ -1,6 +1,22 @@
 package ru.gb.gryaznov.lesson7;
 
 public class Cat {
-
+    private String name;
+    private int apetite;
+    private boolean satiety;
+    public Cat(String name, int apetite, boolean satiety){
+        this.name=name;
+        this.apetite=apetite;
+        this.satiety=false;
     }
+    public void eat(Plate p){
+        if(p.getFood()>=apetite) {
+            p.decreaseFood(apetite);
+            this.satiety = true;
+            System.out.println(this.name +"("+this.apetite + ")" + " поел: - " + this.satiety + " осталось.еды: " + p.getFood());
+        }else {
+            System.out.println(this.name +"("+this.apetite + ")" + " поел: - " + this.satiety + " осталось.еды: " + p.getFood());
+        }
+    }
+
 }
